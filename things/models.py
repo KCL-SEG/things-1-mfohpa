4384b17c09e13053bb67ba8b5b4becb2e90ef82b
mfohpa <mfohpa@gmail.com>
2023-12-05 12:15:55 +0000

    add refinements to things model

--- things/models.py
+++ things/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=520)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.TextField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
